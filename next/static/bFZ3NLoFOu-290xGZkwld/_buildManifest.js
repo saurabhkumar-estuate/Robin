ad137c87eaf9bd0fd83aaf67d7c098a9db314e1f
Saurabh Kumar <SauKumar@ESTUATE.COM>
2023-11-10 18:09:36 +0530

    next

---
+++ next/static/bFZ3NLoFOu-290xGZkwld/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/css/51d154659b5c9fda.css","static/chunks/pages/index-0463568c881f7a2d.js"],"/_error":["static/chunks/pages/_error-ee5b5fb91d29d86f.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
